a267218108d9a7950e2da21b9e649103ec1c6c57
Macka323 <martin.milchevski2@gmail.com>
2023-06-24 17:42:13 +0200

    *

--- DirectoryServer/Controllers/WeatherForecastController.cs
+++ DirectoryServer/Controllers/WeatherForecastController.cs
@@ -10,12 +10,34 @@ namespace DirectoryServer.Controllers {
         [HttpGet("getfile")]
         public async Task<IActionResult> Download(string fileName) {
 
-            FileStream stream = new FileStream(fileName,FileMode.Open);
+            if(!IsFileNameValid(fileName)) {
+                return BadRequest();
+            }
+
+            var stream = new FileStream(fileName,FileMode.Open);
 
             if(stream == null)
                 return NotFound(); // returns a NotFoundResult with Status404NotFound response.
 
             return File(stream, "application/octet-stream", Path.GetFileName(fileName)); // returns a FileStreamResult
+        }
+
+        private bool IsFileNameValid(string file) {
+            try {
+                if(string.IsNullOrEmpty(file))
+                    return false;
+
+                
+                if(file.IndexOfAny(Path.GetInvalidFileNameChars()) >= 0)
+                    return false;
+
+                return true;
+
+            }
+            catch {
+                return false;
+            }
+            return false;
         }
     }
 }
